c44da1046683d2cdc6bbc82896b712a44938161b
Roger David Alba Ortega <99060136+Davidalbort@users.noreply.github.com>
2023-01-25 22:03:42 -0500

    Add some styles and import all styles of app

--- src/containers/Product.tsx
+++ src/containers/Product.tsx
@@ -1,39 +1,49 @@
 import { Heading } from "@/components/Heading"
 import { Images } from "@/components/Images"
 import { Information } from "@/components/Information"
+import { Loading } from "@/components/Loading"
 import { Stats } from "@/components/Stats"
 import { Types } from "@/components/Types"
 import { useProvider } from "@/hooks/useProvider"
+import styles from "@/styles/containers/Product.module.scss"
 
 export const Product = () => {
 	const product= useProvider()
 	return(
-		<section>
-			{product?.pokemon?
+		<section className={styles.container}>
+			{(product?.loading && !product.message)?
 				(
 					<>
-						<Heading 
-							title={product.pokemon.name}
-							typeHeading="h2"
-						/>
-						<Images 
-							source={product?.pokemon?.image}
-							name={product?.pokemon.name}
-						/>
-						<Information 
-							weight={product.pokemon.weight}
-							height={product.pokemon.height}
-						/>
-						<Types 
-							types={product.pokemon.types}
-						/>
-						<Stats 
-							stats={product.pokemon.stats}
-							
-						/>
+						<Loading />
 					</>
 				)
-				: null
+				:(product?.pokemon && !product.loading)?
+					(
+						<>
+							<Heading 
+								title={product.pokemon.name}
+								typeHeading="h2"
+							/>
+							<Images 
+								source={product?.pokemon?.image}
+								name={product?.pokemon.name}
+							/>
+							<Information 
+								weight={product.pokemon.weight}
+								height={product.pokemon.height}
+								className={styles.information}
+							/>
+							<Types 
+								types={product.pokemon.types}
+								className={styles.type}
+							/>
+							<Stats 
+								stats={product.pokemon.stats}
+								className={styles.stats}
+							/>
+						</>
+					)
+					:null
 			}
 		</section>
 	)
