d9026513c1f01ab146dba1582e4c9a26f0fff21b
Roger David Alba Ortega <99060136+Davidalbort@users.noreply.github.com>
2023-01-25 22:02:49 -0500

    Refactor(Refactoring): Add handleReset and useProvider

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,22 +1,20 @@
-import { getProductsById } from "@/services/getProductById"
-import { getProductsByName } from "@/services/getProductByName"
+import { useProvider } from "@/hooks/useProvider"
 import React, { useState } from "react"
-
+import styles from "@/styles/components/Form.module.scss"
+import { useRouter } from "next/router"
 interface StateForm {
     id:number
     name: string
     options: string
 }
-
-
-
 export const Form = () => {
 	const [inputId,setInputId] = useState<StateForm["id"]>()
 	const [inputName,setInputName] = useState<StateForm["name"]>("")
 	const [option,setOption] = useState<StateForm["options"]>("")
-	const [message,setMessage] = useState<string>("")
+	const product= useProvider()
+	const router= useRouter()
 	const handleChange = (event:React.ChangeEvent<HTMLInputElement>) => {
-		const {name,value} = event.target
+		const {value} = event.target
 		if(option==="id"){
 			setInputId(Number(value))
 		}
@@ -32,53 +30,62 @@ export const Form = () => {
 		if(option){
 			if(option==="id"){
 				if(inputId){
-					getProductsById({id:inputId}).then(response => console.log(response))
+					if(inputId<1){
+						product?.updateMessage("Write number greater than 0")
+					}
+					product?.getPokemonById(inputId)
 				}
 			}else{
-				getProductsByName({name:inputName}).then(response => console.log(response))
+				const name = inputName.toLocaleLowerCase()
+				product?.getPokemonByName(name)
 			}
 		}
 		else{
-			setMessage("please select the id or name button before clicking the search button, thanks")
+			product?.updateMessage("please select the id or name button before clicking the search button, thanks")
 		}
-        
 	}
+	const handleReset = () => {
+		router.reload()
+	}
+	console.log(typeof(inputId))
 	return (
 
-		<form onSubmit={handleSubmit}>
+		<form onSubmit={handleSubmit} className={styles.container}>
+			<h1>Pokedex</h1>
 			<fieldset>
 				<legend>Search by</legend>
-				<label htmlFor="id">ID</label>
 				<input type="radio" name="options" id="id" value="id" onChange={handleChangeOptions}/>
-				<label htmlFor="name">NAME</label>
+				<label htmlFor="id">ID</label>
 				<input type="radio" name="options" id="name" value="name" onChange={handleChangeOptions}/>
+				<label htmlFor="name">NAME</label>
 			</fieldset>
 			{
 				option==="id" ?
-					(<input 
-						type="number"
+					(<input
+						type={"number"}
 						name={"id"}
 						onChange={handleChange}
 						value={inputId}
 						required
 						placeholder="Write number id of pokemon"
+						title="Write only number"
 					/>)
 					:
-					(<input 
+					(<input
 						name={"name"}
 						onChange={handleChange}
 						value={inputName}
 						required
 						pattern={"[A-Za-z]+"}
 						placeholder="Write name of pokemon"
+						title="Write only letters not numbers"
 					/>)
 			}
-			
-			{message ?
+			{product?.message ?
 				(
-					<section>
-						<span>X</span>
-						<p>{message}</p>
+					<section className={styles.error}>
+						<span onClick={handleReset}>X</span>
+						<p>{product?.message}</p>
 					</section>
 				)
 				:null}
